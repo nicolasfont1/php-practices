c20cd32dbbd56e59a63919d5a1f6f01e6c5e656e
nicolasfont1 <nicolasfont15@gmail.com>
2024-05-07 15:53:57 -0300

    indtroducing conditionals

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php 
   const NAME = 'Nicolas';
+  $age = 23;
   $job = 'developer';
   $output = NAME . ' works as ' . $job . ' and is learning';
 
   define('LOGO_URL', 'https://www.php.net/images/logos/new-php-logo.svg');
+
+  $isOlder = $age > 18;
 ?>
 
 <h2>
   <?= $output ?>
 </h2>
 <img src="<?= LOGO_URL?>" alt="PHP Logo" width="200">
+<h4>
+  He is older than 18? <?= var_export($isOlder) ?>
+</h4>
 
 <style>
   :root {
