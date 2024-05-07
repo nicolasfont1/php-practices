693e6f496e6cbaf410122978e40d089d8e1eb13e
nicolasfont1 <nicolasfont15@gmail.com>
2024-05-07 11:42:58 -0300

    First Hello world

---
+++ index.php
@@ -0,0 +1,15 @@
+<h1>
+  <?= 'Hello world!'; ?>
+</h1>
+
+<style>
+  :root {
+    color-scheme: light dark;
+  }
+
+  body {
+    display: grid;
+    place-content: center;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+</style>
