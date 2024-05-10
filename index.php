79606015976e8f334e59166d25895b32ead528fa
nicolasfont1 <nicolasfont15@gmail.com>
2024-05-10 15:48:32 -0300

    associative arrays

--- index.php
+++ index.php
@@ -6,20 +6,26 @@
 
   define('LOGO_URL', 'https://www.php.net/images/logos/new-php-logo.svg');
 
-  $programmingLanguages = ['PHP', 'Java', 'JavaScript', 'C', 'Pascal', 'Cobol'];
-  $programmingLanguages[] = 'EasterEgg';
+  $person = [
+    'name' => 'Nicolás',
+    'age' => 23,
+    'job' => 'developer',
+    'sports' => ['football', 'basketball']
+  ];
 ?>
 
 <h2><?= $output ?></h2>
 <div style="margin: auto;">
   <img src="<?= LOGO_URL?>" alt="PHP Logo" width="200">
 </div>
-<h2><?= 'List of programming languages: ' ?></h2>
+<h2>Things about <?= $person["name"] ?></h2>
 <ul>
-  <?php foreach ($programmingLanguages as $key => $language) : ?>
-    <li><?= $key . ' ' . $language ?></li>
-  <?php endforeach ?>
+  <li><?= 'Age: ' . $person["age"] ?></li>
+  <li><?= 'Work as: ' . $person["age"] ?></li>
+  <li><?= 'He likes to play: ' . $person["sports"][1] ?></li>
+  <li><?= 'And also ' . $person["sports"][0] ?></li>
 </ul>
+
 
 <style>
   :root {
