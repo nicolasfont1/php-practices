721a4f19c39f7613a9ee92435e0bf761127fc9db
nicolasfont1 <nicolasfont15@gmail.com>
2024-05-15 19:20:51 -0300

    introducing bootstrap

--- index.php
+++ index.php
@@ -1,38 +1,69 @@
+<head>
+  <title>PHP Practices</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+
 <?php
-$url = 'https://whenisthenextmcufilm.com/api';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+const API_URL = 'https://whenisthenextmcufilm.com/api';
+$ch = curl_init(API_URL);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Me guardo el resultado en vez de imprimirlo en pantalla.
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Apago verificaciones SSL.
 $result = curl_exec($ch);
 
-if ($result === false) {
-  $error = curl_error($ch);
-  echo 'Error: ' . $error;
-} else {
+if ($result !== false) {
   $data = json_decode($result, true);
+
 }
 
 curl_close($ch);
 ?>
 
-<main>
-  <h2>Next Marvel movie</h2>
-  <img src="<?= $data["poster_url"]; ?>" width="300" alt="Poster de <?= $data["title"]; ?>">
-</main>
+<body>
+  <?php if ($data) : ?>
+  <main class="mainContainer">
+      <section class="movieInfoSection">
+        <h4 class="text-muted">Next Marvel movie</h4>
+        <h2><?= $data["title"] ?></h2>
+      </section>
+      
+
+
+
+
+  <?php else : ?>
+    <main style="display: grid; place-content: center; height: 100vh">
+      <h2>Something went wrong :/</h2>
+    </main>
+  <?php endif; ?>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
 
 <style>
-  :root {
-    color-scheme: dark;
+  body {
+    background-color: #000;
+    color: whitesmoke;
   }
 
-  body {
-    display: grid;
-    place-content: center;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  .mainContainer {
+    background-image: url(<?= $data["poster_url"] ?>);
+    width: 100%;
+    max-width: 500px;
+    height: 100vh;
+    margin: 0 auto;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .movieInfoSection {
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
   }
 </style>
-
-<head>
-  <title>PHP Practices</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
